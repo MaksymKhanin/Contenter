ad3bfbb698347edf1ebfbe100cba461dbdca204b
Максим Ханин <trablohin@gmail.com>
2018-12-18 16:11:38 +0200

    аудио айфрейм  сделал

--- Contenter/Models/Audio.cs
+++ Contenter/Models/Audio.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Contenter.Models
 {
     public class Audio
     {
-        public int Id { get; set; }
-        public string Link { get; set; }
+        [Key]
+        public int AudioId { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Duration { get; set; }
     }
 }
